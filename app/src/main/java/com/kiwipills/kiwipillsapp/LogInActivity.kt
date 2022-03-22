2b86907e79d92d8fa4b0979954dd5f100483fec7
Ale Rodríguez <alejandra.rodriguezub@uanl.edu.mx>
2022-03-21 23:22:07 -0600

    Pantalla de medicamento nuevo

--- app/src/main/java/com/kiwipills/kiwipillsapp/LogInActivity.kt
+++ app/src/main/java/com/kiwipills/kiwipillsapp/LogInActivity.kt
@@ -1,11 +1,11 @@
 package com.kiwipills.kiwipillsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
     }
 }
