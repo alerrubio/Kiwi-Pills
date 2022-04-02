e650fdd56080b9e597e95d52c339d54c8ccb6b4d
FuigerCoba <89951566+FuigerCoba@users.noreply.github.com>
2022-04-01 19:58:45 -0600

    Pillbox, Drawer, etc

--- app/src/main/java/com/kiwipills/kiwipillsapp/MainActivity.kt
+++ app/src/main/java/com/kiwipills/kiwipillsapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.kiwipills.kiwipillsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_drawer)
+
+        val drawer = findViewById<DrawerLayout>(R.id.elDrawer)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+        val toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.app_name, R.string.app_name)
+
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
     }
 }
--- app/src/main/java/com/kiwipills/kiwipillsapp/NewMedicineActivity.kt
+++ app/src/main/java/com/kiwipills/kiwipillsapp/NewMedicineActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class NewMedicineActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_new_medicine)
     }
 }
