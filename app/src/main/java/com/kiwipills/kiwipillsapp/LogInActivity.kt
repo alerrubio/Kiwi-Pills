5b5a4b09e9670673ab52f623b26bbf5ab90dc51d
Ale Rodríguez <alejandra.rodriguezub@uanl.edu.mx>
2022-04-02 12:54:06 -0600

    Agregué intents a LogInActivity para el cambio de pantallas

--- app/src/main/java/com/kiwipills/kiwipillsapp/LogInActivity.kt
+++ app/src/main/java/com/kiwipills/kiwipillsapp/LogInActivity.kt
@@ -1,11 +1,20 @@
 package com.kiwipills.kiwipillsapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnEntrar = findViewById<Button>(R.id.btn_loginLog)
+        btnEntrar.setOnClickListener { view ->
+            val  activityIntent =  Intent(this,MainActivity::class.java)
+            startActivity(activityIntent)
+        }
     }
 }
